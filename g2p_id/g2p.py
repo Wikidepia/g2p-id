bc9324a1e6d0ac8fcea836131e0f77d3c8f43c59
Akmal <magchuz@pm.me>
2022-05-29 08:33:50 +0700

    Use ONNX to predict PUEBI pronounciation

--- g2p_id/g2p.py
+++ g2p_id/g2p.py
@@ -2,14 +2,33 @@ import json
 import os
 import re
 
-import torch
-from dp.phonemizer import Phonemizer
+import numpy as np
+import onnxruntime
 from nltk.tokenize import TweetTokenizer
 
 from .syllable_splitter import SyllableSplitter
 
 word_tokenize = TweetTokenizer().tokenize
 dirname = os.path.dirname(__file__)
+
+
+class Predictor:
+    def __init__(self, model_path):
+        # fmt: off
+        self.text_vocab = ["[PAD]", "[START]", "[END]", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.phoneme_vocab = ["[PAD]", "[START]", "[END]", 'a', 'b', 'c', 'd', 'ê', 'è', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        # fmt: on
+        self.session = onnxruntime.InferenceSession(model_path)
+
+    def predict(self, word):
+        text = [1] + [self.text_vocab.index(c) for c in word] + [2]
+        text.extend([0] * (28 - len(text)))  # Pad to 28 tokens
+        indices, _ = self.session.run(None, {"text": [text]})
+
+        output = indices[0]
+        (stop_len,) = np.where(output == self.phoneme_vocab.index("[END]"))
+        prediction = output[1 : stop_len[0]].tolist()
+        return "".join([self.phoneme_vocab[i] for i in prediction])
 
 
 class G2P:
@@ -22,10 +41,8 @@ class G2P:
         with open(map_path) as f:
             self.map = json.load(f)
 
-        model_path = os.path.join(dirname, "id_kbbi_autoreg.pt")
-        self.phonemizer = Phonemizer.from_checkpoint(model_path)
-        model = self.phonemizer.predictor.model
-        self.phonemizer.predictor.model = torch.jit.script(model)
+        model_path = os.path.join(dirname, "tiny-pron.onnx")
+        self.predictor = Predictor(model_path)
 
         self.syllable_splitter = SyllableSplitter()
 
@@ -42,11 +59,11 @@ class G2P:
             elif "e" not in word or not word.isalpha():
                 pron = word
             elif "e" in word:
-                pron = self.phonemizer(word, lang="id")
+                pron = self.predictor.predict(word)
 
             # [ALOFON] o or ô
             if "o" in word:
-                sylls = self.syllable_splitter.split_syllables("onde")
+                sylls = self.syllable_splitter.split_syllables(pron)
                 for i, syll in enumerate(sylls):
                     if "o" in syll:
                         if syll[-1] != "o":  # Posisi tertutup
