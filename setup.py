352e442956deba0b30e67ffe414e42de7d940f56
Akmal <magchuz@pm.me>
2023-01-30 15:45:04 +0700

    Update version to 0.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="g2p-id",
     packages=find_packages(exclude=[]),
-    version="0.0.7",
+    version="0.0.8",
     license="MIT",
     description="Indonesian Grapheme-to-Phoneme (G2P)",
     author="Akmal",
